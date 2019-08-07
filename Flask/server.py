f8a131d914dd14580d2a7ee88ed5a8405e033659
Carl Gordon <33558245+bklynlyphe1@users.noreply.github.com>
2019-08-07 15:39:51 -0400

    Update server.py

--- Flask/server.py
+++ Flask/server.py
@@ -3,27 +3,27 @@ import pandas as pd
 import requests
 import openpyxl
 
-df = pd.read_csv("/jojolion/static/train.csv")
+df = pd.read_csv("/Flask/static/train.csv")
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    df = pd.read_csv("/jojolion/static/train.csv")
+    df = pd.read_csv("/Flask/static/train.csv")
     df = df.head()
     return render_template('home.html', tables=[df.to_html()])
 
 
 @app.route('/reverse_odd')
 def reverse():
-    df = pd.read_csv("/jojolion/static/train.csv")
+    df = pd.read_csv("/Flask/static/train.csv")
     reverse = df[df.columns[::-1]]
-    reverse.to_csv("/jojolion/static/reverse.csv")
+    reverse.to_csv("/Flask/static/reverse.csv")
     reverse = pd.read_csv("/jojolion/static/reverse.csv", index_col=0)
     reverse = reverse.head()
     odd_cols = df[df.columns[::2]]
-    odd_cols.to_csv("/jojolion/static/odd_cols.csv")
-    odd_cols = pd.read_csv("/jojolion/static/odd_cols.csv", index_col=0)
+    odd_cols.to_csv("/Flask/static/odd_cols.csv")
+    odd_cols = pd.read_csv("/Flask/static/odd_cols.csv", index_col=0)
     odd_cols = odd_cols.head()
     return render_template('reverse_odd.html', tables=[reverse.to_html(), odd_cols.to_html()], titles=['na', 'Reversed Columns', 'Odd Columns'])
 
@@ -37,7 +37,7 @@ def msft():
         result = r.json()
         msft = pd.DataFrame(result['Time Series (5min)'])
         msft = msft.transpose()
-        msft.to_excel("/jojolion/static/msft_dailyquotes.xlsx")
+        msft.to_excel("/Flask/static/msft_dailyquotes.xlsx")
         msft = msft.head()
         return render_template('msft.html', tables=[msft.to_html()])
 
